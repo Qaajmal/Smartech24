bae96eb88e4ee6aff8575c02d0ba2e7a3708c702
ajmal <qaajmal@gmail.com>
2022-05-31 22:23:44 -0400

    Smart

---
+++ Smarttechbatch-24/src/test/java/com/practice/SecondTest.java
@@ -0,0 +1,25 @@
+package com.practice;
+
+public class SecondTest {
+
+	public static void main(String[] args) {
+	//boolean abc = false;
+	// 100=computer, 50=phone, 20=watch
+		
+		int a = 20;
+		
+		if(a==100) {
+			System.out.println("computer");
+		}
+
+		else if (a==50) {
+			
+			System.out.println("phone");
+		}
+		
+		else if (a==20) {
+			System.out.println("watch");
+		}
+	}
+	
+}
